24bbb83ac4234a7c1ffb64b0d1dd85541b299072
Aboli Aradhye <aboli.a.aradhye@gmail.com>
2017-09-26 08:41:41 +0530

    Assignment 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,17 @@
-$(document).ready(function(){
-	$("#mycarousel").carousel({interval:3000});
-	$("#carousel-button").click(function(){
-		if ($("#carousel-button").children("span").hasClass("fa-pause")){
-			$("#mycarousel").carousel("pause");
-			$("#carousel-button").children("span").addClass("fa-play");
-			$("#carousel-button").children("span").removeClass("fa-pause");
-		} 
-		else{
-			$("#mycarousel").carousel("cycle");
-			$("#carousel-button").children("span").addClass("fa-pause");
-			$("#carousel-button").children("span").removeClass("fa-play");
-		}
-	});
-
-	$("#reservebtn").click(function(){
-		$("#reservemodal").modal();
-	});
-	
-	$("#login").click(function(){
-		$("#loginmodal").modal();
-	});
-});
+   <script>
+   $("#log").click(function(){$("#loginModal").modal('show')});
+    $("#but").click(function(){$("#x").modal('show')});
+        
+    $("#carousel-button").click(function(){
+        if($("#carousel-button").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause')};
+            $("#carousel-button").children("span").removeClass('fa-pause');
+            $("#carousel-button").children("span").addClass('fa-play');
+        }
+        else if ($("#carousel-button").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle')};
+            $("#carousel-button").children("span").removeClass('fa-play');
+            $("#carousel-button").children("span").addClass('fa-pause');                    
+        }
+    });
+    </script>
